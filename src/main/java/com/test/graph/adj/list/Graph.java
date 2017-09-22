f499cb0a08122ea3f9021536028e231b3b8b5bee
rakesh <rakeshpriyad@gmail.com>
2017-09-22 14:00:34 +0530

    first commit

---
+++ src/main/java/com/test/graph/adj/list/Graph.java
@@ -0,0 +1,30 @@
+package com.test.graph.adj.list;
+
+import java.util.LinkedList;
+
+class Graph {
+	int numVertices;
+	private LinkedList<Integer> adjLists[];
+
+	@SuppressWarnings("unchecked")
+	Graph(int vertices) {
+		numVertices = vertices;
+		adjLists = new LinkedList[vertices];
+
+		for (int i = 0; i < vertices; i++)
+			adjLists[i] = new LinkedList<>();
+	}
+
+	void addEdge(int src, int dest) {
+		adjLists[src].add(dest);
+	}
+
+	public static void main(String args[]) {
+		Graph g = new Graph(4);
+
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 3);
+	}
+}
